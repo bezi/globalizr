9762ef104e11ca25d2c64aed3f315497b4b6bfc7
Oscar Bezi <oscar.bezi@gmail.com>
2013-11-09 08:17:56 -0500

    Heroku static files fixes.

--- globalizr/wsgi.py
+++ globalizr/wsgi.py
@@ -2,8 +2,8 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "globalizr.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#Heroku static files
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
