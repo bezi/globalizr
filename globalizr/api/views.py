6081f086941667b579b8938eb9a30d9ebbf4fe0b
Oscar Bezi <oscar.bezi@gmail.com>
2013-11-10 07:24:56 -0500

    Bugfixes.

--- globalizr/api/views.py
+++ globalizr/api/views.py
@@ -7,14 +7,17 @@ from django.http import HttpResponse
 
 INTERFACE_DIR = os.path.abspath(os.path.dirname(__file__)) + '/interfaces/json/'
 INTERFACES = ["flickr", "weather"];
-def interface(request, interface, disc="default"):
-    if interface.lower() in INTERFACES:
-        response = open(INTERFACE_DIR + interface.lower() + '.json').read()
+def interface(request, inter, disc="default"):
+    print "SHIT GOT CALLED {}".format(inter)
+    if inter.lower() in INTERFACES:
+        print "WE ARE GOOD TO FUCKING GO"
+        response = open(INTERFACE_DIR + inter.lower() + '.json').read()
     else:
-        data = { "status": 1 }
+        print "FUCKIN {} AINT IN {}".format(inter, INTERFACES)
+        data = { "status": 1, "key": inter }
         response = json.dumps(data, sort_keys=True)
     return HttpResponse(response, mimetype='application/json')
 
 def error(request):
-    response = {"error: faulty API call"}
+    response = {"error": "faulty API call"}
     return HttpResponse(json.dumps(response), mimetype='application/json')
